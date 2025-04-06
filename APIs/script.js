4dbe214cc9c05e1da08ba6494438492e1846e3c1
Krishn-10 <sharmakr_2@rknec.edu>
2025-04-06 22:01:01 +0530

    Using fetch with await keyword

--- APIs/script.js
+++ APIs/script.js
@@ -8,22 +8,41 @@
 //     age : 21 
 // }
 
-let fact = "https://catfact.ninja/fact";
-fetch(fact)
-    .then((res) => {
-        return res.json();
-    })
-    .then((data1)=>{
-        console.log("data 1 : ", data1.fact);
-        return fetch(fact);
-    })
-    .then((res)=>{
-        return res.json();
-    })
-    .then((data2)=>{
-        console.log("Data 2 : ", data2.fact);
-    })
-    .catch((err) => {
-        console.log("Error: ", err);
-    })
+// let fact = "https://catfact.ninja/fact";
+// fetch(fact)
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((data1)=>{
+//         console.log("data 1 : ", data1.fact);
+//         return fetch(fact);
+//     })
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data2)=>{
+//         console.log("Data 2 : ", data2.fact);
+//     })
+//     .catch((err) => {
+//         console.log("Error: ", err);
+//     })
 
+//Using fetch with await keyword
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(res);
+        console.log(data.fact);
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(res2);
+        console.log(data2.fact);
+    }
+    catch(e) {
+        console.log("Error : ", e);
+    }
+    console.log("bye");
+}
